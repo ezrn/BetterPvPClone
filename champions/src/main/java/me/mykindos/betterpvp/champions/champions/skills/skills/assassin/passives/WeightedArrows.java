1ec7890a7ef17bfc37a5e3820169107ac56ddd7c
ezrn <zanderaguirre@icloud.com>
2023-09-22 00:53:22 -0700

    improve arrow ability

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/WeightedArrows.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/assassin/passives/WeightedArrows.java
@@ -1,38 +1,39 @@
 package me.mykindos.betterpvp.champions.champions.skills.skills.assassin.passives;
 
 
-import java.util.Optional;
+import java.util.Iterator;
+import java.util.WeakHashMap;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
-import me.mykindos.betterpvp.champions.champions.skills.data.SkillWeapons;
 import me.mykindos.betterpvp.champions.champions.skills.types.PassiveSkill;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
 import me.mykindos.betterpvp.core.components.champions.events.PlayerCanUseSkillEvent;
+import me.mykindos.betterpvp.core.framework.updater.UpdateEvent;
 import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilMath;
-import me.mykindos.betterpvp.core.utilities.UtilPlayer;
 import me.mykindos.betterpvp.core.utilities.UtilServer;
-import org.bukkit.Effect;
 import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.util.Vector;
 
 @Singleton
 @BPvPListener
 public class WeightedArrows extends Skill implements PassiveSkill, Listener {
 
+    private final WeakHashMap<Arrow, Location> arrows = new WeakHashMap<>();
 
+    private double baseDamage;
     @Inject
     public WeightedArrows(Champions champions, ChampionsManager championsManager) {
         super(champions, championsManager);
@@ -48,7 +49,25 @@ public class WeightedArrows extends Skill implements PassiveSkill, Listener {
 
         return new String[]{
                 "The closer you are to your target, the more damage your arrows deal",
-                "Maximum Damage: <val>" + (5 + level) + "0%"};
+                "Maximum Damage: <val>" + (3 + (2 * level))};
+    }
+
+    @UpdateEvent
+    public void update() {
+        Iterator<Arrow> it = arrows.keySet().iterator();
+        while (it.hasNext()) {
+            Arrow next = it.next();
+            if (next == null) {
+                it.remove();
+            } else if (next.isDead()) {
+                it.remove();
+            } else {
+                Location location = next.getLocation().add(new Vector(0, 0.25, 0));
+                Particle.SCULK_CHARGE_POP.builder().location(location).receivers(60).extra(0).spawn();
+
+            }
+        }
+
     }
 
     @EventHandler
@@ -74,10 +93,10 @@ public class WeightedArrows extends Skill implements PassiveSkill, Listener {
 
         Location loc = arrows.remove(arrow);
         double length = UtilMath.offset(loc, event.getDamagee().getLocation());
-        double damage = Math.min(baseDamage + getLevel(damager), length / 3.0 - 4);
+        double damage = Math.min(baseDamage + getLevel(damager), 3.0 / (length+0.1) - 4);
 
         event.setDamage(event.getDamage() + (damage));
-        event.setReason(getName() + (length > deathMessageThreshold ? " <gray>from <green>" + (int) length + "<gray> blocks" : ""));
+        event.setReason(getName());
 
     }
 
@@ -91,5 +110,7 @@ public class WeightedArrows extends Skill implements PassiveSkill, Listener {
         return SkillType.PASSIVE_A;
     }
 
-
+    public void loadSkillConfig(){
+        baseDamage = getConfig("baseDamage", 13.0, Double.class);
+    }
 }
