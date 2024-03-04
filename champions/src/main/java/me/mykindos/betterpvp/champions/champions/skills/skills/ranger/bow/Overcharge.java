62f8d0fc0ab30cd489528f925a3bce57d82bbaf8
zande <zanderaguirre@icloud.com>
2024-03-04 14:11:05 -0800

    overcharge stuff

--- champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/Overcharge.java
+++ champions/src/main/java/me/mykindos/betterpvp/champions/champions/skills/skills/ranger/bow/Overcharge.java
@@ -3,12 +3,15 @@ package me.mykindos.betterpvp.champions.champions.skills.skills.ranger.bow;
 import com.destroystokyo.paper.ParticleBuilder;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import me.mykindos.betterpvp.champions.Champions;
 import me.mykindos.betterpvp.champions.champions.ChampionsManager;
 import me.mykindos.betterpvp.champions.champions.skills.Skill;
+import me.mykindos.betterpvp.champions.champions.skills.data.ChargeData;
 import me.mykindos.betterpvp.champions.champions.skills.data.SkillActions;
 import me.mykindos.betterpvp.champions.champions.skills.types.InteractSkill;
+import me.mykindos.betterpvp.core.client.gamer.Gamer;
 import me.mykindos.betterpvp.core.combat.events.CustomDamageEvent;
 import me.mykindos.betterpvp.core.components.champions.Role;
 import me.mykindos.betterpvp.core.components.champions.SkillType;
@@ -17,8 +20,10 @@ import me.mykindos.betterpvp.core.listener.BPvPListener;
 import me.mykindos.betterpvp.core.utilities.UtilBlock;
 import me.mykindos.betterpvp.core.utilities.UtilMessage;
 import me.mykindos.betterpvp.core.utilities.UtilTime;
+import me.mykindos.betterpvp.core.utilities.model.display.DisplayComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
@@ -45,22 +50,18 @@ import java.util.WeakHashMap;
 @BPvPListener
 public class Overcharge extends Skill implements InteractSkill, Listener {
 
-    private final WeakHashMap<Player, OverchargeData> data = new WeakHashMap<>();
-    private final WeakHashMap<Arrow, Integer> bonus = new WeakHashMap<>();
-    private final List<Arrow> arrows = new ArrayList<>();
-    private final Set<UUID> charging = new HashSet<>();
+    private final WeakHashMap<Player, ChargeData> charging = new WeakHashMap<>();
+    private final WeakHashMap<Arrow, ArrowData> bonus = new WeakHashMap<>();
 
-    private double baseDamageIncrement;
 
-    private double damageIncrementPerLevel;
-
-    private double durationIncrement;
-    
-    private double durationIncrementPerLevel;
+    private final DisplayComponent actionBarComponent = ChargeData.getActionBar(this,
+            charging,
+            gamer -> true);
 
     private double baseMaxDamage;
-
     private double maxDamageIncreasePerLevel;
+    private double baseCharge;
+    private double chargeIncreasePerLevel;
 
     @Inject
     public Overcharge(Champions champions, ChampionsManager championsManager) {
@@ -78,16 +79,13 @@ public class Overcharge extends Skill implements InteractSkill, Listener {
         return new String[]{
                 "Hold right click with a Bow to use",
                 "",
-                "Draw back harder on your bow, giving",
-                "<stat>" + baseDamageIncrement + "</stat> bonus damage per <stat>" + durationIncrement + "</stat> seconds",
+                "Draw back harder on your bow, gaining <val>" + getChargePerSecond(level),
+                "charge per second",
                 "",
-                "Maximum Damage: <val>" + getMaxDamage(level)
+                "At <stat>100%</stat> charge you will deal <val>" + getMaxDamage(level) + "</val> extra damage",
         };
     }
 
-    public double getDamageIncrement(int level) {
-        return baseDamageIncrement + ((level-1) * damageIncrementPerLevel);
-    }
 
     public double getMaxDamage(int level) {
         return baseMaxDamage + ((level-1) * maxDamageIncreasePerLevel);
@@ -99,126 +97,144 @@ public class Overcharge extends Skill implements InteractSkill, Listener {
     }
 
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-        charging.remove(event.getPlayer().getUniqueId());
-
+    @Override
+    public void trackPlayer(Player player, Gamer gamer) {
+        gamer.getActionBar().add(900, actionBarComponent);
     }
 
+    @Override
+    public void invalidatePlayer(Player player, Gamer gamer) {
+        gamer.getActionBar().remove(actionBarComponent);
+    }
+
+    @Override
+    public void activate(Player player, int level) {
+        final ChargeData chargeData = new ChargeData((float) getChargePerSecond(level) / 100);
+        charging.put(player, chargeData);
+        Bukkit.getLogger().info("[Overcharge] Activated for player " + player.getName() + " with level " + level + " and initial CPS " + getChargePerSecond(level));
+    }
+
+    private double getChargePerSecond(int level) {
+        return baseCharge + (chargeIncreasePerLevel * (level - 1)); // Increment of 10% per level
+    }
 
     @EventHandler
     public void onPlayerShoot(EntityShootBowEvent event) {
         if (!(event.getEntity() instanceof Player player)) return;
         if (!(event.getProjectile() instanceof Arrow arrow)) return;
-        charging.remove(player.getUniqueId());
-        if (hasSkill(player)) {
-            OverchargeData overchargeData = data.get(player);
-            if (overchargeData != null) {
-                bonus.put(arrow, overchargeData.getCharge());
-                data.remove(player);
-            }
-        }
 
+        ChargeData chargeData = charging.remove(player);
+        Bukkit.getLogger().info("removing player from chargeData");
+        if (chargeData != null) {
+            double chargePercentage = chargeData.getCharge();
+            int level = getLevel(player); // Assuming there's a method to get the player's level
+            double extraDamage = getMaxDamage(level) * chargePercentage;
+            bonus.put(arrow, new ArrowData(extraDamage, level));
+            Bukkit.getLogger().info("[Overcharge] Player " + player.getName() + " shot an arrow with extra damage: " + extraDamage);
+        } else {
+            Bukkit.getLogger().info("[Overcharge] Player " + player.getName() + " shot an arrow without charging.");
+        }
     }
+
 
     @UpdateEvent
     public void createRedDustParticles() {
-        bonus.forEach((arrow, bonusDamage) -> {
-            if (arrow.isValid() && !arrow.isDead() && !arrow.isOnGround() && bonus.get(arrow) > 0) {
+        // Create a list to hold arrows that need to be removed after iteration
+        List<Arrow> toRemove = new ArrayList<>();
 
+        bonus.forEach((arrow, arrowData) -> {
+            if (arrow.isValid() && !arrow.isDead() && !arrow.isOnGround()) {
                 double baseSize = 0.25;
-                double count = (bonus.get(arrow));
-
-                double finalSize = baseSize * count;
+                double finalSize = baseSize * arrowData.extraDamage;
 
                 Particle.DustOptions redDust = new Particle.DustOptions(Color.fromRGB(255, 0, 0), (float)finalSize);
-                new ParticleBuilder(Particle.REDSTONE)
-                        .location(arrow.getLocation())
-                        .count(1)
-                        .offset(0.1, 0.1, 0.1)
-                        .extra(0)
-                        .data(redDust)
-                        .receivers(60)
-                        .spawn();
+                arrow.getWorld().spawnParticle(Particle.REDSTONE, arrow.getLocation(), 1, 0.1, 0.1, 0.1, 0, redDust);
+            } else {
+                // Add arrows to the removal list instead of removing them directly
+                toRemove.add(arrow);
             }
         });
 
-        bonus.keySet().removeIf(arrow -> !arrow.isValid() || arrow.isDead() || arrow.isOnGround());
+        // Remove the collected arrows from the map after iteration
+        toRemove.forEach(bonus::remove);
     }
+
+
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDamage(CustomDamageEvent event) {
         if (!(event.getProjectile() instanceof Arrow arrow)) return;
-        if (!(event.getDamager() instanceof Player)) return;
-        if (bonus.containsKey(arrow)) {
-            event.setDamage(event.getDamage() + bonus.get(arrow));
+        ArrowData arrowData = bonus.remove(arrow); // Now retrieving ArrowData instead of Double
+        if (arrowData != null) {
+            double extraDamage = arrowData.extraDamage; // Extracting extraDamage from ArrowData
+            event.setDamage(event.getDamage() + extraDamage);
+            Bukkit.getLogger().info("[Overcharge] Applying extra damage: " + extraDamage);
             event.addReason(getName());
         }
     }
 
     @UpdateEvent
-    public void updateOvercharge() {
-        Iterator<Map.Entry<Player, OverchargeData>> iterator = data.entrySet().iterator();
+    public void updateCharge() {
+        // Charge check
+        Iterator<Player> iterator = charging.keySet().iterator();
         while (iterator.hasNext()) {
-            OverchargeData data = iterator.next().getValue();
-            Player player = Bukkit.getPlayer(data.getUuid());
-            if (player != null) {
-
-                if (!charging.contains(player.getUniqueId())) {
-                    iterator.remove();
-                    continue;
-                }
-
-                if (!isHolding(player)) {
-                    iterator.remove();
-                    continue;
-                }
-
-                Material mainhand = player.getInventory().getItemInMainHand().getType();
-                if (mainhand == Material.BOW && player.getActiveItem().getType() == Material.AIR) {
-                    iterator.remove();
-                    continue;
-                }
-
-                if (mainhand == Material.CROSSBOW && player.getActiveItem().getType() == Material.AIR) {
-                    CrossbowMeta meta = (CrossbowMeta) player.getInventory().getItemInMainHand().getItemMeta();
-                    if (!meta.hasChargedProjectiles()) {
-                        iterator.remove();
-                    }
-                    continue;
-                }
-
-                if (UtilBlock.isInLiquid(player)) {
-                    iterator.remove();
-                    continue;
-                }
-
-                if (UtilTime.elapsed(data.getLastCharge(), (long) (durationIncrement * 1000))) {
-                    if (data.getCharge() < data.getMaxCharge()) {
-                        data.addCharge();
-                        UtilMessage.simpleMessage(player, getClassType().getName(), "%s: <yellow>+%d<gray> Bonus Damage", getName(), data.getCharge());
-                        player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 0.4F, 1.0F + 0.05F * data.getCharge());
-                    }
-                }
+            Player player = iterator.next();
+            ChargeData charge = charging.get(player);
+            if (player == null || !player.isOnline()) {
+                Bukkit.getLogger().info("player is null or offline");
+                iterator.remove();
+                continue;
             }
-        }
 
-        arrows.removeIf(arrow -> arrow.isOnGround() || !arrow.isValid() || arrow.isInsideVehicle());
+            // Remove if they no longer have the skill
+            Gamer gamer = championsManager.getClientManager().search().online(player).getGamer();
+            int level = getLevel(player);
+            if (level <= 0) {
+                Bukkit.getLogger().info("level is < 0");
+                iterator.remove();
+                continue;
+            }
+
+            Material mainhand = player.getInventory().getItemInMainHand().getType();
+            if (mainhand == Material.BOW && player.getActiveItem().getType() == Material.AIR) {
+                Bukkit.getLogger().info("they are holding a bow");
+                iterator.remove();
+                continue;
+            }
+
+            if (mainhand == Material.CROSSBOW && player.getActiveItem().getType() == Material.AIR) {
+                Bukkit.getLogger().info("they have a crossbow");
+                CrossbowMeta meta = (CrossbowMeta) player.getInventory().getItemInMainHand().getItemMeta();
+                if (!meta.hasChargedProjectiles()) {
+                    iterator.remove();
+                }
+                continue;
+            }
+
+            if (UtilBlock.isInLiquid(player)) {
+                Bukkit.getLogger().info("in liquid");
+                iterator.remove();
+                continue;
+            }
+
+            // Check if they still are blocking and charge
+            if (gamer.isHoldingRightClick()) {
+                Bukkit.getLogger().info("they are holding the right thing and holding right click");
+                charge.tick();
+                charge.tickSound(player);
+                continue;
+            } else {
+                Bukkit.getLogger().info("not holding right click or holding right item");
+            }
+            Bukkit.getLogger().info("removing player from iterator");
+            iterator.remove();
+        }
     }
 
 
     @Override
     public SkillType getType() {
-
         return SkillType.BOW;
-    }
-
-    @Override
-    public void activate(Player player, int level) {
-        if (!data.containsKey(player)) {
-            data.put(player, new OverchargeData(player.getUniqueId(), (int) getDamageIncrement(level), (int) getMaxDamage(level)));
-            charging.add(player.getUniqueId());
-        }
     }
 
     @Override
@@ -226,44 +242,33 @@ public class Overcharge extends Skill implements InteractSkill, Listener {
         return SkillActions.RIGHT_CLICK;
     }
 
+
+    @Data
+    @AllArgsConstructor
+    private static class Shoot {
+        private final ChargeData data;
+        private final int level;
+    }
+
+    private static class ArrowData {
+        final double extraDamage;
+        final int level;
+
+        ArrowData(double extraDamage, int level) {
+            this.extraDamage = extraDamage;
+            this.level = level;
+        }
+    }
+
     @Override
     public boolean displayWhenUsed() {
         return false;
     }
 
-    @Data
-    private static class OverchargeData {
-        private final UUID uuid;
-        private final int increment;
-        private final int maxCharge;
-
-        private int charge;
-        private long lastCharge;
-
-        public OverchargeData(UUID uuid, int increment, int maxCharge) {
-            this.uuid = uuid;
-            this.charge = 0;
-            this.lastCharge = System.currentTimeMillis();
-            this.increment = increment;
-            this.maxCharge = maxCharge;
-
-        }
-
-        public void addCharge() {
-            if (getCharge() <= getMaxCharge()) {
-                setCharge(getCharge() + getIncrement());
-                lastCharge = System.currentTimeMillis();
-            }
-        }
-
-    }
-
     public void loadSkillConfig() {
-        baseDamageIncrement = getConfig("baseDamageIncrement", 1.0, Double.class);
-        damageIncrementPerLevel = getConfig("damageIncrementPerLevel", 0.0 , Double.class);
-        durationIncrement = getConfig("baseDurationIncrement", 0.4, Double.class);
-
         baseMaxDamage = getConfig("baseMaxDamage", 2.0, Double.class);
         maxDamageIncreasePerLevel = getConfig("maxDamageIncreasePerLevel", 1.0, Double.class);
+        baseCharge = getConfig("baseCharge", 40.0, Double.class);
+        chargeIncreasePerLevel = getConfig("chargeIncreasePerLevel", 10.0, Double.class);
     }
 }
